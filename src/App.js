7c86dea8358b6dc4b78db42e4cf9bfe870156726
Omkar Jadhav <omkarj@wemotiveforge.com>
2021-04-16 13:53:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <p>{process.env.REACT_APP_BASE_URL}</p>
         </p>
         <a
           className="App-link"
